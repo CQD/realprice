9ab890ac8d1ddb0ba640c5ced31da088022654ea
CQD <cqd@hiigara.net>
2023-08-03 22:55:05 +0800

    改欄位顯示名稱

--- view/index.php
+++ view/index.php
@@ -36,7 +36,7 @@
 <option value="unit_price_avg">平均單價</option>
 <option value="price_avg">平均房價</option>
 <option value="area_avg">平均坪數</option>
-<option value="cnt">總案量</option>
+<option value="cnt">總交易筆數</option>
 <option value="price_total">總交易額</option>
 </select>
 
@@ -47,7 +47,7 @@
 <option value="unit_price_avg">平均單價</option>
 <option value="price_avg">平均房價</option>
 <option value="area_avg">平均坪數</option>
-<option value="cnt" selected>總案量</option>
+<option value="cnt" selected>總交易筆數</option>
 <option value="price_total">總交易額</option>
 </select>
 
